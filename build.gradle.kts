97acdaa4e3a29847485e9af89edddf3512ebf82a
cfhabel-source <cf.habel@gmail.com>
2026-09-16 21:23:51 +0200

    Prepare Plant Storage for hosting

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,6 +30,8 @@ dependencies {
     implementation("com.google.oauth-client:google-oauth-client-jetty:1.34.1")
 
     implementation("com.google.http-client:google-http-client-jackson2:1.43.3")
+
+    runtimeOnly("org.slf4j:slf4j-simple:2.0.17")
 }
 
 application {
